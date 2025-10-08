d8cc900fcc1a70502dfa889ee70555ddc9181847
shihab20105 <shihabur10@gmail.com>
2025-10-08 23:29:21 +0100

    Went through module summary and Interview questions

---
+++ 39-9 Module Summary And Interview Questions/summary.js
@@ -0,0 +1,14 @@
+/*                      39-9 module summary and Interview questions */
+
+/* 
+Interview questions:
+====================
+* Tell me something about JS engine v8 internal mechanism.
+* What is event loop in JavaScript?
+* If javascript is single threaded, how does it handle asynchronous call?
+* Differences between setTimeOut and SetInterval
+* Can you explain the difference between an async function and a regular function in JavaScript?
+* What's the error handling strategy for promises that were rejected while awaiting?
+* What are the states of a promise?
+* Can you explain what the '.then()' method does?
+*/
